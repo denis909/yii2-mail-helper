d8a6b0c1f1d34addd20b6dbcaf885772984c7fc4
denis909 <denis909@mail.ru>
2020-04-23 14:01:41 +0300

    n/a

---
+++ MailHelper.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace denis909\yii;
+
+use Exception;
+use yii\helpers\ArrayHelper;
+use yii\mail\MessageInterface;
+
+class MailHelper
+{
+
+    public static function hasRecipient(MessageInterface $message, $toEmail = null, $toName = null)
+    {
+        if (!$toEmail && !$toName)
+        {
+            return true;
+        }
+
+        foreach($message->getTo() as $email => $name)
+        {
+            $valid = true;
+
+            if ($toEmail && ($toEmail != $email))
+            {
+                $valid = false;
+            }
+
+            if ($toName && ($toName != $name))
+            {
+                $valid = false;
+            }
+
+            if ($valid)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function isEmailSent(array $messages, array $attributes = [])
+    {
+        foreach($messages as $message)
+        {
+            if (!($message instanceof MessageInterface))
+            {
+                throw new Exception('Message is not an instance of ' . MessageInterface::class);
+            }
+
+            if ($attributes)
+            {
+                $subject = ArrayHelper::getValue($attributes, 'subject');
+
+                if (!$subject || ($subject != $message->getSubject()))
+                {
+                    continue;
+                }
+
+                $toEmail = ArrayHelper::getValue($attributes, 'toEmail');
+
+                $toName = ArrayHelper::getValue($attributes, 'toName');
+
+                if (!static::hasRecipient($message, $toEmail, $toName))
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
